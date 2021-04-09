79db865f0c63a1e9f660a7e2f425cb264d8e9081
彭帅飞 <miyaweb@163.com>
2021-04-09 20:47:57 +0800

    add demo/server.go

---
+++ demo/server/server.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/nacos/registry"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+	"log"
+)
+
+func main() {
+	sc := []constant.ServerConfig{
+		*constant.NewServerConfig("127.0.0.1", 8848),
+	}
+
+	cc := constant.ClientConfig{
+		NamespaceId:         "public", //namespace id
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "/tmp/nacos/log",
+		CacheDir:            "/tmp/nacos/cache",
+		RotateTime:          "1h",
+		MaxAge:              3,
+		LogLevel:            "debug",
+	}
+
+	// a more graceful way to create naming client
+	client, err := clients.NewNamingClient(
+		vo.NacosClientParam{
+			ClientConfig:  &cc,
+			ServerConfigs: sc,
+		},
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	r := registry.New(client)
+
+	app := kratos.New(
+		kratos.Name("helloworld"),
+		kratos.Server(),
+		kratos.Registrar(r),
+	)
+
+	if err := app.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
