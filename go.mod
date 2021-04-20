a6028e7d5a4df724bdd53622cb4220598a6efd50
shenqidebaozi <906604588@qq.com>
2021-04-20 13:20:36 +0800

    nacos config

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/go-kratos/kratos/v2 v2.0.0-20210415063033-9007abfd2888
 	github.com/nacos-group/nacos-sdk-go v1.0.7
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 )
---
+++ test/config_test.go
@@ -0,0 +1,70 @@
+package test
+
+import (
+	"fmt"
+	kconfig "github.com/go-kratos/kratos/v2/config"
+	"github.com/go-kratos/nacos/config"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+	"gopkg.in/yaml.v3"
+	"testing"
+)
+
+func TestGetConfig(t *testing.T) {
+	ip := "mse-874cf952-p.nacos-ans.mse.aliyuncs.com"
+	//ctx := context.Background()
+
+	sc := []constant.ServerConfig{
+		*constant.NewServerConfig(ip, 8848),
+	}
+
+	cc := constant.ClientConfig{
+		NamespaceId:         "5c7d1f8b-6782-46bf-b36a-dfd9cfc14b89", //namespace id
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "/tmp/nacos/log",
+		CacheDir:            "/tmp/nacos/cache",
+		RotateTime:          "1h",
+		MaxAge:              3,
+		LogLevel:            "debug",
+	}
+
+	// a more graceful way to create naming client
+	client, err := clients.NewConfigClient(
+		vo.NacosClientParam{
+			ClientConfig:  &cc,
+			ServerConfigs: sc,
+		},
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c := kconfig.New(
+		kconfig.WithSource(
+			config.NewConfigSource(client, config.Group("private"), config.DataID("go-rpc-executor")),
+			),
+		kconfig.WithDecoder(func(kv *kconfig.KeyValue, v map[string]interface{}) error {
+			return yaml.Unmarshal(kv.Value, v)
+		}),
+	)
+
+	if err := c.Load(); err != nil {
+		panic(err)
+	}
+
+	name, err := c.Value("logger.level").String()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("get value",name)
+	// 监听值内容变更
+	done := make(chan bool)
+	c.Watch("logger.level", func(key string, value kconfig.Value) {
+		fmt.Println(key ," value change", value)
+		done <- true
+	})
+	<-done
+}
